ca4c6e1cdc3af1ac389b020c2fe2639184ea037f
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-08-26 16:55:37 -0300

    Reset the tip calculator when the reset button is clicked

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const peopleNum = document.querySelector("#num-of-people");
 
 const tipAmountDisplay = document.querySelector(".tip-amount__number");
 const totalDisplay = document.querySelector(".total__number");
+const resetBtn = document.querySelector(".reset-btn");
 
 function getSelectedTip() {
   if (customTipInput.value) {
@@ -50,10 +51,28 @@ function displayResults(totalPerPerson, tipAmountPerPerson) {
   totalDisplay.textContent = `$${totalPerPerson}`;
 }
 
+function enableBtn(resetBtn) {
+  resetBtn.classList.remove("disabled");
+}
+
+function resetCalc() {
+  billInput.value = "";
+  customTipInput.value = "";
+  tipButtons[2].classList.add("checked");
+  peopleNum.value = "";
+  resetBtn.classList.add("disabled");
+  tipAmountDisplay.textContent = "$0.00";
+  totalDisplay.textContent = "$0.00";
+}
+
 peopleNum.addEventListener("input", () => {
   if (peopleNum.value >= 1 && billInput.value) {
     const results = calcTip();
     displayResults(results.totalPerPerson, results.tipAmountPerPerson);
+    enableBtn(resetBtn);
+    resetBtn.addEventListener("click", () => {
+      resetCalc();
+    });
   }
 });
 
